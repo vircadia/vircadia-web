85291fe34c2fa8fca95ff966254f992876392c98
Kalila L <somnilibertas@gmail.com>
2021-07-16 02:35:28 -0400

    Re-init vircadia-web w/ Quasar CLI w/ TS support

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,41 +1,31 @@
-<!--
-//  Error404.vue
-//
-//  Created by Kalila L. on May 9th, 2021.
-//  Copyright 2021 Vircadia contributors.
-//
-//  Distributed under the Apache License, Version 2.0.
-//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
--->
-
 <template>
-    <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-        <div>
-            <div style="font-size: 30vh">
-                404
-            </div>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        404
+      </div>
 
-            <div class="text-h2" style="opacity:.4">
-                Oops. Nothing here...
-            </div>
+      <div class="text-h2" style="opacity:.4">
+        Oops. Nothing here...
+      </div>
 
-            <q-btn
-                class="q-mt-xl"
-                color="white"
-                text-color="blue"
-                unelevated
-                to="/"
-                label="Go Home"
-                no-caps
-            />
-        </div>
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/"
+        label="Go Home"
+        no-caps
+      />
     </div>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'Error404'
-});
+  name: 'Error404'
+})
 </script>
