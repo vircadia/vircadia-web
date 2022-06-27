6ab007efc8b7850f8f706eb829bb557171bd9ab0
nolan <nolancloudms@gmail.com>
2022-06-27 10:10:17 +0800

    Modify code headers

--- src/modules/avatar/AvatarController.ts
+++ src/modules/avatar/AvatarController.ts
@@ -1,9 +1,14 @@
-/*
-//  Copyright 2021 Vircadia contributors.
+//
+//  AvatarController.ts
+//
+//  Created by Nolan Huang on 17 Jun 2022.
+//  Copyright 2022 Vircadia contributors.
+//  Copyright 2022 DigiSomni LLC.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-*/
+//
+
 import {
     Vector3,
     Mesh,
