37b02f5afe939c18f3e56ee3ad39f6d3bc2fc67d
nolan <nolancloudms@gmail.com>
2022-06-27 10:10:17 +0800

    Modify code headers

--- src/modules/avatar/index.ts
+++ src/modules/avatar/index.ts
@@ -1,8 +1,12 @@
-/*
-//  Copyright 2021 Vircadia contributors.
+//
+//  index.ts
+//
+//  Created by Nolan Huang on 17 Jun 2022.
+//  Copyright 2022 Vircadia contributors.
+//  Copyright 2022 DigiSomni LLC.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-*/
+//
 
 export { AvatarController } from "./AvatarController";
