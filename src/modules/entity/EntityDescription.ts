e5d91afe68b9ec694ea5183949b57127f5cbd4d7
nolan <nolancloudms@gmail.com>
2022-07-26 17:39:35 +0800

    Add EntityBuilder classes

---
+++ src/modules/entity/EntityDescription.ts
@@ -0,0 +1,18 @@
+//
+//  EntityProperties.ts
+//
+//  Created by Nolan Huang on 26 Jul 2022.
+//  Copyright 2022 Vircadia contributors.
+//  Copyright 2022 DigiSomni LLC.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+import { IEntityProperties } from "./EntityProperties";
+
+export interface IEntityDescription {
+    DataVersion : string;
+    Id: string;
+    Version: string;
+    Entities: IEntityProperties[];
+}
