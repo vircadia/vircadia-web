2b81c2dc6b85d43a6893be18a27635b350093a6b
Giga <gigabyte5671@gmail.com>
2022-09-01 20:13:35 +1200

    Create DefaultModels.ts

---
+++ src/modules/avatar/DefaultModels.ts
@@ -0,0 +1,43 @@
+//
+//  DefaultModels.ts
+//
+//  Created by Giga on 1 Sep 2022.
+//  Copyright 2022 Vircadia contributors.
+//  Copyright 2022 DigiSomni LLC.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+export const DefaultActiveAvatarModel = "HTP45FSQ";
+
+export const DefaultAvatarModels = {
+    HTP45FSQ: {
+        name: "Sara",
+        image: "https://staging.vircadia.com/O12OR634/UA92/sara-cropped-small.webp",
+        file: "https://staging.vircadia.com/O12OR634/UA92/sara.glb",
+        scale: 1,
+        starred: true
+    },
+    EPS62RC9: {
+        name: "Tiffany",
+        image: "",
+        file: "https://staging.vircadia.com/O12OR634/Avatars/Tiffany.glb",
+        scale: 1,
+        starred: true
+    },
+    AIOUPXVY: {
+        name: "Martha",
+        image: "",
+        file: "https://staging.vircadia.com/O12OR634/Avatars/Martha.glb",
+        scale: 1,
+        starred: true
+    },
+    LRX76LNL: {
+        name: "Miles",
+        image: "",
+        file: "https://staging.vircadia.com/O12OR634/Avatars/Miles.glb",
+        scale: 1,
+        starred: true
+    }
+};
