4f4039460c0083dafe2de3eac4cd3d460a6df4b5
Giga <gigabyte5671@gmail.com>
2023-06-28 09:06:27 +1200

    Remove explicit port from Quasar config to silence dev-server port warning

--- quasar.config.js
+++ quasar.config.js
@@ -127,7 +127,6 @@ module.exports = configure(function(ctx) {
         // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
         devServer: {
             https: false,
-            port: 8080,
             open: true // opens browser window automatically
         },
 
