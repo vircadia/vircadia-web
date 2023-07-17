0836aea2c43d79b81202ba0ee95117c1bafeba25
Giga <gigabyte5671@gmail.com>
2023-07-17 23:21:45 +1200

    Tidy and document the Places module
    - Convert Places from an object literal to a static class for clarity.
    - Add TSDoc comments.

--- src/modules/places/index.ts
+++ src/modules/places/index.ts
@@ -1,10 +1,10 @@
-/*
+//
 //  Copyright 2021 Vircadia contributors.
 //  Copyright 2022 DigiSomni LLC.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-*/
+//
 
 import { MetaverseManager } from "@Modules/metaverse";
 import { API } from "@Modules/metaverse/API";
@@ -20,32 +20,37 @@ export interface PlaceEntry {
     currentAttendance: number;
 }
 
-export const Places = {
-
-    async getActiveList(): Promise<PlaceEntry[]> {
+/**
+ * Static methods for interacting with places/worlds in the connected Metaverse.
+ */
+export class Places {
+    /**
+     * @returns A list of the places (worlds) available in the connected Metaverse.
+     */
+    public static async getActiveList(): Promise<PlaceEntry[]> {
         const places: PlaceEntry[] = [];
-        if (MetaverseManager.activeMetaverse?.isConnected) {
-            try {
-                const placesResponse = await API.get(API.endpoints.places + "?status=online") as GetPlacesResponse;
 
-                placesResponse.places.forEach((place) => {
-                    places.push({
-                        name: place.name,
-                        placeId: place.placeId,
-                        address: place.address,
-                        description: place.description,
-                        thumbnail: place.thumbnail,
-                        currentAttendance: place.current_attendance === undefined ? 0 : place.current_attendance
-                    } as PlaceEntry);
+        if (!MetaverseManager.activeMetaverse?.isConnected) {
+            Log.error(Log.types.PLACES, "Attempted to get places when not connected to a Metaverse server.");
+        }
+
+        try {
+            const placesResponse = await API.get(API.endpoints.places + "?status=online") as GetPlacesResponse;
+
+            for (const place of placesResponse.places) {
+                places.push({
+                    name: place.name,
+                    placeId: place.placeId,
+                    address: place.address,
+                    description: place.description,
+                    thumbnail: place.thumbnail ?? "",
+                    currentAttendance: place.current_attendance ?? 0
                 });
-            } catch (error) {
-                const errorMessage = findErrorMessage(error);
-                Log.error(Log.types.PLACES, `Exception while attempting to get places: ${errorMessage}`);
             }
-        } else {
-            Log.error(Log.types.PLACES, "Attempt to get places when metaverse not connected");
+        } catch (error) {
+            Log.error(Log.types.PLACES, `Exception while attempting to get places: ${findErrorMessage(error)}`);
         }
 
         return places;
     }
-};
+}
