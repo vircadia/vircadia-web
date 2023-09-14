569e707427667b7bb754e0f6004700e0f343f90d
Giga <gigabyte5671@gmail.com>
2023-09-14 12:16:27 +1200

    Update attribution

--- src/modules/utility/hysteresis.ts
+++ src/modules/utility/hysteresis.ts
@@ -1,3 +1,14 @@
+//
+//  hysteresis.ts
+//
+//  Created by Giga on 4 September 2023.
+//  Copyright 2023 Vircadia contributors.
+//  Copyright 2023 DigiSomni LLC.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
 /**
  * The default number of points to sample from.
  */
